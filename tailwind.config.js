dfcd3d44c4e488ff9f80d82fb40f7c9e984ebbf3
Vaisakh Pradeep <92vaisakh92@gmail.com>
2022-08-16 12:16:29 +0530

    feat: custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     extend: {},
+    colors: {
+      'ft-black': '#060606',
+      'ft-purple-100': '#B070F1',
+      'ft-purple-200': '#9D60DB',
+      'ft-purple-300': '#9F54FF',
+      'ft-purple-400': '#883DE7',
+      'ft-blue-100': '#6D85FF',
+      'ft-blue-200': '#384FCD',
+      'ft-grey-100': '#8C8DAF'
+    },
   },
   plugins: [],
 }
